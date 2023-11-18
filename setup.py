8e22f42703b5d7dd7063197094c6369d8922a573
Furechan <furechan@xsmail.com>
2023-11-17 22:35:57 -0500

    ...

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from setuptools import setup, find_packages, Extension
 
+# TODO remove MANIFEST.in ?
+
 srcdir = "src"
 package_dir = {"": srcdir}
 packages = find_packages(where=srcdir)
@@ -22,5 +24,5 @@ ext_modules = [make_extension(f) for f in Path(srcdir).rglob('*.pyx')]
 setup(
     packages=packages,
     package_dir=package_dir,
-    ext_modules=ext_modules
-)
+    ext_modules=ext_modules,
+) 
